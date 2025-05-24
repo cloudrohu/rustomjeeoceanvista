2beb98a4ee8490a0c7c10b58fc3bfd19d84fdb24
cloudrohu <141917927+cloudrohu@users.noreply.github.com>
2025-05-24 11:07:44 +0530

    EMAIL_BACKEND

--- rustomjeeoceanvista/settings.py
+++ rustomjeeoceanvista/settings.py
@@ -126,6 +126,13 @@ USE_TZ = True
 
 
 import os
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'dncontroller.com@gmail.com'
+EMAIL_HOST_PASSWORD = 'ikfmvqlokbagepog'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
